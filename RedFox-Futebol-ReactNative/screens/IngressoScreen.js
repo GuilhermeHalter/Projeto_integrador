934f374c1216b3963f35b981e66e85f989005899
daniel <danielmoggi2019@gmail.com>
2023-09-20 14:33:45 -0300

    Ingresso

--- RedFox-Futebol-ReactNative/screens/IngressoScreen.js
+++ RedFox-Futebol-ReactNative/screens/IngressoScreen.js
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const IngressoScreen = () => {
-  
   const navigation = useNavigation();
-
-  // Estados separados para rastrear a quantidade de cada tipo de ingresso
-  const [selectedPrices, setSelectedPrices] = useState({
-    padrao: 0,
-    vip: 0,
-    premium: 0,
-  });
 
   const [ticketQuantity, setTicketQuantity] = useState({
     padrao: 0,
@@ -19,32 +11,38 @@ const IngressoScreen = () => {
     premium: 0,
   });
 
-  useEffect(() => {
-    // Calcula o total somando os preços de todos os tipos de ingresso
-    const total = selectedPrices.padrao + selectedPrices.vip + selectedPrices.premium;
-    setSelectedPrices({ ...selectedPrices, total });
-  }, [selectedPrices]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const handleSelectTicket = (price, type) => {
-    const newSelectedPrices = { ...selectedPrices };
-    newSelectedPrices[type] = price * ticketQuantity[type];
-    setSelectedPrices(newSelectedPrices);
-  };
-
-  const incrementQuantity = (type) => {
-    setTicketQuantity({
-      ...ticketQuantity,
-      [type]: ticketQuantity[type] + 1,
-    });
+    const typeTotal = price * ticketQuantity[type];
+    setTicketQuantity((prevQuantity) => ({
+      ...prevQuantity,
+      [type]: prevQuantity[type] + 1,
+    }));
   };
 
   const decrementQuantity = (type) => {
-    if (ticketQuantity[type] > 1) {
-      setTicketQuantity({
-        ...ticketQuantity,
-        [type]: ticketQuantity[type] - 1,
-      });
+    if (ticketQuantity[type] > 0) {
+      const typeTotal = selectedPrices[type] * (ticketQuantity[type] - 1);
+      setTicketQuantity((prevQuantity) => ({
+        ...prevQuantity,
+        [type]: prevQuantity[type] - 1,
+      }));
     }
+  };
+
+  const addTotalToPrice = () => {
+    let total = 0;
+    Object.keys(selectedPrices).forEach((type) => {
+      total += selectedPrices[type] * ticketQuantity[type];
+    });
+    setTotalPrice(total);
+  };
+
+  const selectedPrices = {
+    padrao: 50,
+    vip: 100,
+    premium: 150,
   };
 
   return (
@@ -52,86 +50,39 @@ const IngressoScreen = () => {
       <Text style={styles.title}>Compra de Ingresso</Text>
       <View style={styles.ticketContainer}>
         <Text style={styles.ticketTitle}>Escolha o tipo de ingresso:</Text>
-        {/* Ingresso Padrão */}
-        <View style={styles.ticketOptionContainer}>
-          <TouchableOpacity
-            style={styles.ticketOption}
-            onPress={() => handleSelectTicket(50, 'padrao')}
-          >
-            <Text style={styles.optionText}>Ingresso Padrão</Text>
-            <Text style={styles.optionPrice}>R$ 50</Text>
-          </TouchableOpacity>
-          <View style={styles.quantityContainer}>
+        {Object.keys(selectedPrices).map((type) => (
+          <View style={styles.ticketOptionContainer} key={type}>
             <TouchableOpacity
-              style={styles.quantityButton}
-              onPress={() => decrementQuantity('padrao')}
+              style={styles.ticketOption}
+              onPress={() => handleSelectTicket(selectedPrices[type], type)}
             >
-              <Text style={styles.quantityButtonText}>-</Text>
+              <Text style={styles.optionText}>Ingresso {type}</Text>
+              <Text style={styles.optionPrice}>R$ {selectedPrices[type]}</Text>
             </TouchableOpacity>
-            <Text style={styles.quantityText}>{ticketQuantity.padrao}</Text>
-            <TouchableOpacity
-              style={styles.quantityButton}
-              onPress={() => incrementQuantity('padrao')}
-            >
-              <Text style={styles.quantityButtonText}>+</Text>
-            </TouchableOpacity>
+            <View style={styles.quantityContainer}>
+              <TouchableOpacity
+                style={styles.quantityButton}
+                onPress={() => decrementQuantity(type)}
+              >
+                <Text style={styles.quantityButtonText}>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantityText}>{ticketQuantity[type]}</Text>
+              <TouchableOpacity
+                style={styles.quantityButton}
+                onPress={() => handleSelectTicket(selectedPrices[type], type)}
+              >
+                <Text style={styles.quantityButtonText}>+</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <Text style={styles.total}>Total: R$ {selectedPrices.padrao}</Text>
-        </View>
-        {/* Ingresso VIP */}
-        <View style={styles.ticketOptionContainer}>
-          <TouchableOpacity
-            style={styles.ticketOption}
-            onPress={() => handleSelectTicket(100, 'vip')}
-          >
-            <Text style={styles.optionText}>Ingresso VIP</Text>
-            <Text style={styles.optionPrice}>R$ 100</Text>
-          </TouchableOpacity>
-          <View style={styles.quantityContainer}>
-            <TouchableOpacity
-              style={styles.quantityButton}
-              onPress={() => decrementQuantity('vip')}
-            >
-              <Text style={styles.quantityButtonText}>-</Text>
-            </TouchableOpacity>
-            <Text style={styles.quantityText}>{ticketQuantity.vip}</Text>
-            <TouchableOpacity
-              style={styles.quantityButton}
-              onPress={() => incrementQuantity('vip')}
-            >
-              <Text style={styles.quantityButtonText}>+</Text>
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.total}>Total: R$ {selectedPrices.vip}</Text>
-        </View>
-        {/* Ingresso Premium */}
-        <View style={styles.ticketOptionContainer}>
-          <TouchableOpacity
-            style={styles.ticketOption}
-            onPress={() => handleSelectTicket(150, 'premium')}
-          >
-            <Text style={styles.optionText}>Ingresso Premium</Text>
-            <Text style={styles.optionPrice}>R$ 150</Text>
-          </TouchableOpacity>
-          <View style={styles.quantityContainer}>
-            <TouchableOpacity
-              style={styles.quantityButton}
-              onPress={() => decrementQuantity('premium')}
-            >
-              <Text style={styles.quantityButtonText}>-</Text>
-            </TouchableOpacity>
-            <Text style={styles.quantityText}>{ticketQuantity.premium}</Text>
-            <TouchableOpacity
-              style={styles.quantityButton}
-              onPress={() => incrementQuantity('premium')}
-            >
-              <Text style={styles.quantityButtonText}>+</Text>
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.total}>Total: R$ {selectedPrices.premium}</Text>
-        </View>
-        {/* Total Geral */}
-        <Text style={styles.totalGeral}>Total Geral: R$ {selectedPrices.total}</Text>
+        ))}
+        <TouchableOpacity
+          style={styles.addTotalButton}
+          onPress={addTotalToPrice}
+        >
+          <Text style={styles.addTotalButtonText}>Adicionar ao Total Geral</Text>
+        </TouchableOpacity>
+        <Text style={styles.totalGeral}>Total Geral: R$ {totalPrice}</Text>
       </View>
     </View>
   );
@@ -204,17 +155,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginHorizontal: 10,
   },
-  total: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 5,
-    color: '#333',
-  },
   totalGeral: {
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 20,
     color: '#333',
+  },
+  addTotalButton: {
+    backgroundColor: '#007BFF',
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  addTotalButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
   },
 });
 
